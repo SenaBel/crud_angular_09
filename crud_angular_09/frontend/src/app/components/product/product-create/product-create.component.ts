48476cc2da0b1eb078902fc3ce258319627cc16a
Abel <abelsena1@hotmail.com>
2020-08-14 17:18:25 -0300

    Connection with the back-end

---
+++ crud_angular_09/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import {Router} from '@angular/router'
+import { Product } from '../product.model';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product ={
+    name: "",
+    price: null
+  }
+
+  
+  constructor(private productService: ProductService,
+   private router: Router ) { }
+
+  ngOnInit(): void {
+    
+  }
+  createProduct(): void {
+    if(this.product.name === "" || this.product.price === null )
+    return this.productService.showMessage('Preencha os Dados')
+    
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto Criado!!')
+      this.router.navigate(['/products'])
+    })
+   
+  }
+
+  cancelProduct(): void {
+    this.router.navigate(['/products'])
+  }
+}
